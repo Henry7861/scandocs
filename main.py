d739765e5733a8ffa39874d98cb2a1b68f659782
HanYeoung <zz0908@knu.ac.kr>
2023-05-08 14:33:03 +0900

    changed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utils
  
 ########################################################################
 webCamFeed = True
-pathImage = "/Users/henry/Desktop/IMG_4796.JPG"
+pathImage = "/home/hanyoung/Documents/scandocs/IMG_4796.JPG"
 cap = cv2.VideoCapture(0)
 cap.set(10,160)
 heightImg = 640
@@ -73,9 +73,10 @@ while True:
  
     stackedImage = utils.stackImages(imageArray,0.75,lables)
     cv2.imshow("Result",stackedImage)
+    cv2.waitKey(0)
  
     # # SAVE IMAGE WHEN 's' key is pressed
-    # if cv2.waitKey(1) &amp; 0xFF == ord('s'):
+    # if cv2.waitKey(1) & 0xFF == ord('s'):
     #     cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
     #     cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
     #                   (1100, 350), (0, 255, 0), cv2.FILLED)
